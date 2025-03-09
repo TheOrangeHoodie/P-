e374295359f290cacbf93e80d6ba540f7e288f04
TheOrangeHoodie <supremethedev@gmail.com>
2025-03-09 09:49:12 +0100

    Added file reading

--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
 import sys
 
+class WrongUseException(Exception):
+    def __init__(self):
+        super().__init__("Wrong use! Correct use... \npython3 main.py <filePath>")
+
 class Compiler:
     def __init__(self, args):
         self.args = args
-        print(args[0])
+        if len(args) != 2:
+            raise WrongUseException()
+        self.fileContents = None
+        with open(args[1], "rt") as file:
+            self.fileContents = file.read()
+
+        print(self.fileContents)
+        
+    def run(self):
         pass
+
 
 
 if __name__ == "__main__":
     args = sys.argv
-    compiler = Compiler()
-    compiler.__init__(args)
+    compiler = Compiler(args)
+    compiler.run()
